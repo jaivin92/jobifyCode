ba0fff26a819ff4f82d6a2762a31567257defbfc
Jaivin <kishan9.3mungara@gmail.com>
2023-04-03 20:31:36 +0530

    resolve

--- controller/authControl.js
+++ controller/authControl.js
@@ -45,9 +45,27 @@ const login = async (req, res) =>{
     res.status(StatusCodes.OK).send({user, token, location:user.location})
 } 
 
-const update = (req, res) =>{
-    res.send('update user')
-    user.save()
+const update = async (req, res) =>{
+
+    const {email,name, lastName, location} = req.body
+    if(!email || !name || !lastName || !location){
+        throw new BadRequestError('Please provide all values')
+    }
+
+    const user = await User.findOne({_id:req.user.userId});
+
+    user.email = email;
+    user.name = name;
+    user.lastName = lastName;
+    user.location = location;
+
+    await user.save()
+
+    const token = user.createJWT()
+    
+    console.log(req.user)
+    //res.send('updateUser')
+    res.status(StatusCodes.OK).send({user, token, location:user.location})
 } 
 
 export {register, login,update}
