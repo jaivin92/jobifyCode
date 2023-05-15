80a5918bc1443acfa395e4c819f144f010685c2e
Jaivin <kishan9.3mungara@gmail.com>
2023-05-15 16:27:22 +0530

    resolve

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,19 @@ import express from 'express'
 
 const router = express.Router()
 
+import rateLimiter from 'express-rate-limit'
+
+const appLimit = rateLimiter({
+    windowMs : 15*60*1000,  //15 minute
+    max : 10,
+    message : 'Too many request this IP address, please try again after 15 minute',
+})
+
 import { register, login,update } from '../controller/authControl.js'
 import authenticateUser from '../middleware/auth.js'
 
-router.route('/register').post(register)
-router.route('/login').post(login)
+router.route('/register').post( appLimit,register)
+router.route('/login').post(appLimit, login)
 router.route('/update').patch(authenticateUser, update)
 router.route('/test').get(login)
 
--- server.js
+++ server.js
@@ -6,6 +6,17 @@ dotenv.config()
 //error handle
 import 'express-async-errors'
 import morgan from 'morgan'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
+
+
 //db and auth
 import connectDB from './db/connect.js'
 
@@ -21,8 +32,16 @@ if(process.env.NODE_ENV !== "production"){
     app.use(morgan('dev'))
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+app.use(express.static(path.resolve(__dirname,'./client/build')))
+
+
 //app.use(cors())
 app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
+
 
 app.get('/', (req,res)=>{
    // throw new Error('error')
@@ -37,6 +56,14 @@ app.get('/api', (req,res)=>{
 app.use('/api/auth',authRouter)
 app.use('/api/job',authenticateUser, jobsRouter)
 //app.use('/api/job', jobsRouter)
+
+// production
+// only when ready to deploy
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
+
+  
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 const port = process.env.PORT || 5000
